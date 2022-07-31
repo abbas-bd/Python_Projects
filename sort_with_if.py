7f12c2b0bd892867910e71cd167224aa5e5fb034
Abbas.bd <110333052+abbas-bd@users.noreply.github.com>
2022-07-31 18:27:30 -0400

    Update sort_with_if.py

--- sort_with_if.py
+++ sort_with_if.py
@@ -2,10 +2,12 @@
 #
 #This program takes 3 input numbers from the user and displays them as smaller to larger numbers in the output.and can also recognize equal numbers.
 #NOTE:This program is written using condition only. To better understand the "IF" command in Python
+print ()
+print ("enter '3' input number:")
 num1 = int(input("input 1: "))
 num2 = int(input("input 2: "))
 num3 = int(input("input 3: "))
-print()
+print ()
 ##
 if num1 < num2 < num3 :
      print (num1,num2,num3)
