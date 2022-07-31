3f1d4b9fe812344c45908331eb040bf44b52abd7
Abbas.bd <110333052+abbas-bd@users.noreply.github.com>
2022-07-31 17:20:20 -0400

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+#!/bin/python3
+#
+#This program takes an input from the user and determines whether it is even or odd
+data = int(input("Enter The Number: "))
+if data % 2 == 0 :
+   print("adad zoj ast")
+else :
+   print("adad fard ast")
---
+++ usr_pass.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+#
+#this program takes input from user and determine password from user, its correct or not
+USR = input("Enter Username: ")
+print(USR)
+PASS = input("Enter password ")
+if PASS == "1234" :
+    print("congratulations, you enter correct password ! :)")
+else:
+    print("incorrect password")
